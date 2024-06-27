e50b8c3b8e5c940462dec619d3734c16d6001309
YYsvg <rara10202001@gmail.com>
2024-06-27 21:02:44 +0900

    create_save

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,4 +1,12 @@
 class PokemonsController < ApplicationController
+  def index
+    @pokemons = Pokemon.all
+  end
+
+  def show
+  end
+
+
   def new
   end
   
@@ -8,8 +16,26 @@ class PokemonsController < ApplicationController
     raw_response = Faraday.get("https://pokeapi.co/api/v2/pokemon/#{params[:search]}")
     if raw_response.status == 200
       @response = JSON.parse(raw_response.body)
+      @pokemon = Pokemon.new(order: @response["id"], name: @response["name"], image_url: @response["sprites"]["front_default"])
     else
       redirect_to new_pokemon_path, notice: "エラー！"
     end
   end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+    
+    if @pokemon.save
+      redirect_to pokemons_path, notice: "「#{@pokemon.name}」をゲットしました。"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def pokemon_params
+    params.require(:pokemon).permit(:order, :name, :image_url)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
 
   get 'pokemons/new', to: 'pokemons#new', as: 'new_pokemon'
   get 'pokemons/fetch_data', to: 'pokemons#fetch_data'
-  
+  get 'pokemons/index', to: 'pokemons#index'
+
+  resources :pokemons, only: [:index,:new,:create]
 end
+
